81c24383fb249ff1da7f16d27176975b0e6f2dde
alexander siemers <alexander.siemers@skf.com>
2015-03-05 12:59:28 +0100

    Minor update

--- MetaModels/OmcOmcPendulum/run.sh
+++ MetaModels/OmcOmcPendulum/run.sh
@@ -11,11 +11,21 @@ fi
 TLMPLUGINDIR=`pwd | awk -F "TLMPlugin" '{print $1 "TLMPlugin"}'`
 if [ -d ${TLMPLUGINDIR}/bin ] ; then
     export PATH=${TLMPLUGINDIR}/bin:$PATH;
+
+    # The following is for OpenModelica start script.
+    CYGDRIVE=`echo $TLMPLUGINDIR | grep cygdrive | grep -v grep`;
+    if [ -z "$CYGDRIVE" ]; then
+        export TLMPluginPath=${TLMPLUGINDIR};
+    else
+        export TLMPluginPath=`echo $TLMPLUGINDIR | awk -F "/" '{print $3 ":/" $4 "/" $5 "/" $6 "/" $7}'`
+    fi
+    echo TLMPluginPath=$TLMPluginPath;
+
 else
     echo "No TLMPlugin/bin found at ${TLMPLUGINDIR}/bin, try anyway...";
 fi
 
 # Start tlmmanager and monitor.
-tlmmanager -p 8666 -m 8667 pendulum.xml&
+tlmmanager -p 8678 -m 8689 pendulum.xml&
 sleep 1;
-tlmmonitor -n 1000 localhost:8667 pendulum.xml
+tlmmonitor -n 1000 172.16.0.100:8689 pendulum.xml
