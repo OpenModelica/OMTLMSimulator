81335e24cde38de48ebe0ebc17a015671fc4ea0a
robbr48 <robbr48@users.noreply.github.com>
2016-09-22 08:36:33 +0200

    Cleaned up some FMILibrary files

--- FMIWrapper1/FMILibrary-2.0.1/build-fmil/CMakeFiles/doc.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "../install/doc/html"
-  "../install/doc/latex"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang)
-  include(CMakeFiles/doc.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
