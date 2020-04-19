0e6f300c1fe0a50fb4c4b24b01d21dcf0de1c5dd
Martin Sjölund <martin.sjolund@liu.se>
2020-04-19 18:41:04 +0200

    Fix endianness for arm64

--- 3rdParty/misc/include/portable_long.h
+++ 3rdParty/misc/include/portable_long.h
@@ -2,7 +2,7 @@
 #define _portable_long_h
 
 // The definition of long64 is moved from portability.h to a separate file.
-// The reason is that including portability.h is unnecssary in 
+// The reason is that including portability.h is unnecssary in
 // many cases, and it is sufficient to use just this file for
 // inclusion to tostr.h
 // This definition was in compress.h
@@ -14,7 +14,7 @@
 #if defined (_MSC_VER)
 #define long64 __int64
 #else
-//! In non-DEC machines 64-bit arithmetic is necessary. 
+//! In non-DEC machines 64-bit arithmetic is necessary.
 typedef  long long int long64 ;
 #endif
 #endif
@@ -27,11 +27,11 @@ typedef  long long int long64 ;
 #if !defined(_LITTLE_ENDIAN) && !defined(_BIG_ENDIAN)
   #if (defined(__LITTLE_ENDIAN) && __LITTLE_ENDIAN)
     #define _LITTLE_ENDIAN
-  #else 
+  #else
     #if (defined(__BIG_ENDIAN) && __BIG_ENDIAN)
       #define _BIG_ENDIAN
     #else
-      #if defined(_X86) || defined(__x86_64) || defined(_M_IX86) || defined(__alpha) || defined(__i386) || defined(_M_X64) || defined(__amd64__) || defined(__arm__)
+      #if defined(_X86) || defined(__x86_64) || defined(_M_IX86) || defined(__alpha) || defined(__i386) || defined(_M_X64) || defined(__amd64__) || defined(__arm__) || defined(__aarch64__)
         #define _LITTLE_ENDIAN
       #else
         #if defined(__sparc) || defined (__mips)
